64796ed7316e18dc3bf025984631f973b5feca96
Nikityyy <110503465+Nikityyy@users.noreply.github.com>
2024-07-29 03:36:17 +0200

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,1 @@
+gsap.registerPlugin(ScrollTrigger);let panels=gsap.utils.toArray(".scroll");panels.forEach(t=>{let e=t.offsetHeight<window.innerHeight?"top top":"bottom bottom";ScrollTrigger.create({trigger:t,start:e,pin:!0,pinSpacing:!1})});
